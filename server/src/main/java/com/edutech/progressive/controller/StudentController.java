71411403e5e3b288b8b87cafb5b47a3220d902cd
Malar1503 <dummy@gmail.com>
2026-01-22 04:19:03 +0000

    Day 6 Task

--- server/src/main/java/com/edutech/progressive/controller/StudentController.java
+++ server/src/main/java/com/edutech/progressive/controller/StudentController.java
@@ -2,14 +2,17 @@ package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Student;
 import com.edutech.progressive.service.impl.StudentServiceImplArraylist;
+import com.edutech.progressive.service.impl.StudentServiceImplJpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,43 +26,94 @@ public class StudentController {
 
     private StudentServiceImplArraylist studentServiceImplArraylist;
 
+    @Autowired
+
+    private StudentServiceImplJpa studentServiceImplJpa;
+
     @GetMapping
 
     public ResponseEntity<List<Student>> getAllStudents() {
 
-        return null;
+        try {
+
+            return new ResponseEntity<>(studentServiceImplJpa.getAllStudents(), HttpStatus.OK);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
 
     }
 
     @GetMapping("/{studentId}")
 
-    public ResponseEntity<Student> getStudentById(int studentId) {
+    public ResponseEntity<Student> getStudentById(@PathVariable int studentId) {
 
-        return null;
+        try {
+
+            return new ResponseEntity<>(studentServiceImplJpa.getStudentById(studentId), HttpStatus.OK);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
 
     }
 
     @PostMapping
 
-    public ResponseEntity<Integer> addStudent(Student student) {
+    public ResponseEntity<Integer> addStudent(@RequestBody Student student) {
 
-        return null;
+        try {
+
+            return new ResponseEntity<>(studentServiceImplJpa.addStudent(student), HttpStatus.CREATED);
+
+            // } catch (RuntimeException e) {
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
 
     }
 
     @PutMapping("/{studentId}")
+    public ResponseEntity<Void> updateStudent(@PathVariable int studentId, @RequestBody Student student) {
 
-    public ResponseEntity<Void> updateStudent(int studentId, Student student) {
+        try {
 
-        return null;
+            student.setStudentId(studentId);
+
+            studentServiceImplJpa.updateStudent(student);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (Exception e) {
+
+            throw new RuntimeException("");
+
+        }
 
     }
 
     @DeleteMapping("/{studentId}")
 
-    public ResponseEntity<Void> deleteStudent(int studentId) {
+    public ResponseEntity<Void> deleteStudent(@PathVariable int studentId) {
 
-        return null;
+        try {
+
+            studentServiceImplJpa.deleteStudent(studentId);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
 
     }
 
